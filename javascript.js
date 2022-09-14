23e79fa03e24a56dbe7c2815c3758d37108681d9
Hakan Bilgi <hknblg34@gmail.com>
2022-09-14 12:45:32 +0300

    selector

---
+++ javascript.js
@@ -0,0 +1,5 @@
+//? Selectors
+const langInput = document.querySelector(".lang-input");
+const addBtn = document.querySelector("#add-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const langList = document.getElementById("lang-list");
