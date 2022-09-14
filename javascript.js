270f4bab0d01a1aa506943c10aeeff268118b55e
Hakan Bilgi <hknblg34@gmail.com>
2022-09-14 12:55:43 +0300

    add-event

--- javascript.js
+++ javascript.js
@@ -3,3 +3,17 @@ const langInput = document.querySelector(".lang-input");
 const addBtn = document.querySelector("#add-btn");
 const deleteBtn = document.querySelector("#delete-btn");
 const langList = document.getElementById("lang-list");
+
+const newUl = document.createElement("ul");
+langList.appendChild(newUl);
+
+//? addBtn event handler
+addBtn.addEventListener("click", () => {
+  if (!langInput.value) {
+    alert("Please enter a language");
+  } else {
+    newUl.innerHTML += `<li>${langInput.value}</li>`;
+    langInput.value = "";
+  }
+  langInput.focus();
+});
